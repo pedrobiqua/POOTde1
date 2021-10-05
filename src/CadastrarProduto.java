eab4caf3c173c156ca9f51a40ca7abc11dc51699
Lukas Jacon Barboza <lukas_jacon@hotmail.com>
2021-10-05 15:53:49 -0300

    aaa

--- src/CadastrarProduto.java
+++ src/CadastrarProduto.java
@@ -1,6 +1,40 @@
 public class CadastrarProduto {
+
     String Descricao;
     int Quantidade;
     Float Preço;
     String Fornecedor;
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return Quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        Quantidade = quantidade;
+    }
+
+    public Float getPreço() {
+        return Preço;
+    }
+
+    public void setPreço(Float preço) {
+        Preço = preço;
+    }
+
+    public String getFornecedor() {
+        return Fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        Fornecedor = fornecedor;
+    }
+
 }
--- src/CadastrarVendedor.java
+++ src/CadastrarVendedor.java
@@ -1,4 +1,5 @@
 public class CadastrarVendedor {
+
     String Nome;
     String Sobrenome;
     String dataDeNascimento;
@@ -11,4 +12,101 @@ public class CadastrarVendedor {
     String DataDeCadastro;
     String login;
     String Senha;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getSobrenome() {
+        return Sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        Sobrenome = sobrenome;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String pais) {
+        Pais = pais;
+    }
+
+    public String getEndereço() {
+        return Endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        Endereço = endereço;
+    }
+
+    public String getDataDeCadastro() {
+        return DataDeCadastro;
+    }
+
+    public void setDataDeCadastro(String dataDeCadastro) {
+        DataDeCadastro = dataDeCadastro;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String senha) {
+        Senha = senha;
+    }
+
 }
