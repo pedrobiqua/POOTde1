0ee8515c03688c0e1ee9e654c304f9bda578d75f
Lukas Jacon Barboza <lukas_jacon@hotmail.com>
2021-10-05 17:53:17 -0300

    Fazendo ListarFornecedor

--- src/ListarFornecedor.java
+++ src/ListarFornecedor.java
@@ -1,10 +1,28 @@
+import java.util.ArrayList;
+
 public class ListarFornecedor {
 
-    public ListarFornecedor(){
-        
+    private ArrayList<CadastrarFornecedor> ListarFornecedor;
+
+    public ListarFornecedor() {
+        this.ListarFornecedor = new ArrayList<CadastrarFornecedor>();
+
     }
 
-    public static void MostrarFornecedor() {
-        
+    public void MostarFornecedor(ArrayList<CadastrarFornecedor> mostra) {
+        for (int i = 0; i < mostra.size(); i++) {
+            System.out.println(mostra.get(i).getNomeFornecedor());
+            System.out.println(mostra.get(i).getRazaoSocial());
+            //por enquanto isso...
+        }
+
+    }
+
+    public void adicCadastrarFornecedor(CadastrarFornecedor CadastrarFornecedor){
+        this.ListarFornecedor.add(CadastrarFornecedor); //Adiciona na ListarFornecedor
+    }
+
+    public ArrayList<CadastrarFornecedor> getListarFornecedor() {
+        return ListarFornecedor;
     }
 }
