50188f0f3be241758bdb7e345b3bc7060adc61ed
Lukas Jacon Barboza <lukas_jacon@hotmail.com>
2021-10-05 15:47:00 -0300

    aaaaa
    add metodo get e set do fornecedor

--- src/CadastrarFornecedor.java
+++ src/CadastrarFornecedor.java
@@ -1,4 +1,5 @@
 public class CadastrarFornecedor {
+
     String NomeFornecedor;
     String RazaoSocial;
     String CNPJ;
@@ -10,4 +11,92 @@ public class CadastrarFornecedor {
     String Endereco;
     int Numero;
     String DataCadastro;
+
+    public String getNomeFornecedor() {
+        return NomeFornecedor;
+    }
+
+    public void setNomeFornecedor(String nomeFornecedor) {
+        NomeFornecedor = nomeFornecedor;
+    }
+
+    public String getRazaoSocial() {
+        return RazaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        RazaoSocial = razaoSocial;
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String pais) {
+        Pais = pais;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        Endereco = endereco;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int numero) {
+        Numero = numero;
+    }
+
+    public String getDataCadastro() {
+        return DataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        DataCadastro = dataCadastro;
+    }
 }
