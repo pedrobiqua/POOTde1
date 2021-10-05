8d52b9f089a136792a268039637fcb217882d9b4
Lukas Jacon Barboza <lukas_jacon@hotmail.com>
2021-10-05 15:38:29 -0300

    add metodo get e set

--- src/CadastrarCliente.java
+++ src/CadastrarCliente.java
@@ -1,4 +1,5 @@
 public class CadastrarCliente {
+
     //Declarando as Variaveis...
     String Nome;
     String Sobrenome;
@@ -12,5 +13,94 @@ public class CadastrarCliente {
     String DataDeCadastro;
     int Numero;
 
+
     //DeclararMetodo
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getSobrenome() {
+        return Sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        Sobrenome = sobrenome;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String pais) {
+        Pais = pais;
+    }
+
+    public String getEndereço() {
+        return Endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        Endereço = endereço;
+    }
+
+    public String getDataDeCadastro() {
+        return DataDeCadastro;
+    }
+
+    public void setDataDeCadastro(String dataDeCadastro) {
+        DataDeCadastro = dataDeCadastro;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int numero) {
+        Numero = numero;
+    }
+
 }
